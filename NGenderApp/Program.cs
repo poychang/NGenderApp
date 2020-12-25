544d7c718d77cf6788033d18fe28f3046178e322
Poy Chang <poypost@gmail.com>
2020-12-26 05:51:19 +0800

    init

---
+++ NGenderApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NGenderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
